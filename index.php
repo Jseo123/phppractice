c90fadf652d69eeb44263fe967627d60fb35a0ab
jseo123 <jose123135@gmail.com>
2021-12-15 12:19:22 +0100

    Validation written

---
+++ index.php
@@ -0,0 +1,17 @@
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="assets\styles.css">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>A Basic HTML5 Template</title>
+</head>
+<body>
+<form action="functions.php" method="POST">
+<label for="fuser">First name:</label><br>
+<input type="text" id="fuser" name="fuser"><br>
+<label for="pass">Password:</label><br>
+<input type="password" id="pass" name="pass"><br>
+<button>Log in</button>
+</form>
+</body>
+</html>
